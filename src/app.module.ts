dd6c7c8d30ba1bf19fcfbaa2befa95fe797d0688
Abunawas <abunawas.arifandi@gmail.com>
2024-09-26 13:26:35 +0700

    course 76

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
+import { TagsModule } from './tags/tags.module';
+import { MetaOptionsModule } from './meta-options/meta-options.module';
 
 @Module({
   imports: [UsersModule, PostsModule, AuthModule,
@@ -14,7 +16,8 @@ import { User } from './users/user.entity';
       inject: [],
       useFactory: () => ({
         type: 'postgres',
-        entities: [User],
+        // entities: [User],
+        autoLoadEntities: true,
         synchronize: true,
         port: 5432,
         username: 'postgres',
@@ -22,7 +25,9 @@ import { User } from './users/user.entity';
         host: 'localhost',
         database: 'nestjs-blog',
       }),
-    })],
+    }),
+    TagsModule,
+    MetaOptionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
